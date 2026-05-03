e0ed36faca30da84ff3a095453efd22960b92347
carlosssricardo <carlossuneo09@gmail.com>
2026-05-03 23:25:02 +0700

    Multiplication Function

--- Calculator.c
+++ Calculator.c
@@ -7,12 +7,16 @@ float addition(float a, float b) {
 }
 
 float subtraction(float a, float b) {
-    float hasil = a - b'
+    float hasil = a - b;
 
     return hasil;
 }
 
-float multiplication(float a, float b) {}
+float multiplication(float a, float b) {
+    float hasil = a * b;
+
+    return hasil;
+}
 
 float division(float a, float b) {}
 
